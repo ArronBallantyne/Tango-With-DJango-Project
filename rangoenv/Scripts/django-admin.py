4188206757374bba13c983ce78f233e83fd0a8f4
Arron Ballantyne <2514426b@student.gla.ac.uk>
2020-04-01 11:54:49 +0100

    Chapter 3 Complete

---
+++ rangoenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\GA1\DJango Projects\tango_with_django_project\rangoenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
