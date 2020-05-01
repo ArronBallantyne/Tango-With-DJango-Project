f6e874a9ab672dd86f99587df959c533ad6346e3
Arron Ballantyne <2514426b@student.gla.ac.uk>
2020-05-01 12:42:29 +0100

    Allowed Hosts

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'l#=2rg+k5v0d$%n^%m^h#67xgcjskzk*9t6l!xhhvxzi$#v(q)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ArronBallantyne.pythonanywhere.com', 'http://127.0.0.1:8000/']
 
 
 # Application definition
